4e3d1ea3c307cf0ab0dedcc6f42431a63a7d79e7
Ning Yap <ning.yap@gmail.com>
2013-07-28 16:07:44 -0400

    integer in schema

--- db/migrate/20130728193134_create_users.rb
+++ db/migrate/20130728193134_create_users.rb
@@ -4,16 +4,16 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :email
-      t.int :extroversion_stat
-      t.int :introversion_stat
-      t.int :sensing_stat
-      t.int :intuition_stat
-      t.int :feeling_stat
-      t.int :thinking_stat
-      t.int :judging_stat
-      t.int :perceiving_stat
-      t.int :type_id
-      t.int :polled_type_id
+      t.integer :extroversion_stat
+      t.integer :integerroversion_stat
+      t.integer :sensing_stat
+      t.integer :integeruition_stat
+      t.integer :feeling_stat
+      t.integer :thinking_stat
+      t.integer :judging_stat
+      t.integer :perceiving_stat
+      t.integer :type_id
+      t.integer :polled_type_id
 
       t.timestamps
     end
