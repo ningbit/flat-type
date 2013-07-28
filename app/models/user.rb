62abceca2135224cee463ea89ce482925cbd0a6f
Ning Yap <ning.yap@gmail.com>
2013-07-28 16:07:21 -0400

    revert back to two models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :email, :extroversion_stat, :introversion_stat, :sensing_stat, :intuition_stat, :feeling_stat, :thinking_stat, :judging_stat, :perceiving_stat, :type_id, :polled_type_id,
+  attr_accessible :first_name, :last_name, :email, :extroversion_stat, :introversion_stat, :sensing_stat, :intuition_stat, :feeling_stat, :thinking_stat, :judging_stat, :perceiving_stat, :type_id, :polled_type_id
 
   belongs_to :type
-  belongs_to :polled_type, :class_name => "Type"
+  # belongs_to :polled_type, :class_name => "Type"
 
 end
