4ffb68bcefaf2cf382f91cf1ccc45467cad93ee9
Ning Yap <ning.yap@gmail.com>
2013-07-28 15:30:37 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlatType::Application.config.secret_token = 'c357ccab6c58e63e1419e1c0f650b6a731fe2b1a8f8aee77251d337384d9b97d73f05bf4d86cced4cb7ca53058a2f3e57350426ba44e581e16c6e52067166ef0'
